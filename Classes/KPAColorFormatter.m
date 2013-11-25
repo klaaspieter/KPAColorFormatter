832278da6fd8a3b7432bc1f264e312a84e58ad05
Klaas Pieter Annema <klaaspieter@annema.me>
2013-11-25 17:28:51 -0500

    Refactor finding the closest matching color into a method

--- Classes/KPAColorFormatter.m
+++ Classes/KPAColorFormatter.m
@@ -28,29 +28,33 @@
 
     NSString *name = [self.colors objectForKey:value];
     if (!name) {
-        UIColor *color = (UIColor *)value;
-        CGFloat red1, green1, blue1;
-        [color getRed:&red1 green:&green1 blue:&blue1 alpha:nil];
-
-        __block CGFloat closestDelta = CGFLOAT_MAX;
-        __block UIColor *closestColor = nil;
-        [[self.colors allKeys] enumerateObjectsUsingBlock:^(UIColor *possibleColor, NSUInteger idx, BOOL *stop) {
-            CGFloat red2, green2, blue2;
-            [possibleColor getRed:&red2 green:&green2 blue:&blue2 alpha:nil];
-            CGFloat deltaR = pow(red2 - red1, 2);
-            CGFloat deltaG = pow(green2 - green1, 2);
-            CGFloat deltaB = pow(blue2 - blue1, 2);
-            CGFloat delta = deltaR + deltaG + deltaB;
-            if (delta < closestDelta) {
-                closestDelta = delta;
-                closestColor = possibleColor;
-            }
-        }];
-
-        name = [self.colors objectForKey:closestColor];
+        name = [self.colors objectForKey:[self colorClosestToColor:value]];
     }
 
     return name;
+}
+
+- (UIColor *)colorClosestToColor:(UIColor *)color;
+{
+    CGFloat red1, green1, blue1;
+    [color getRed:&red1 green:&green1 blue:&blue1 alpha:nil];
+
+    __block CGFloat closestDelta = CGFLOAT_MAX;
+    __block UIColor *closestColor = nil;
+    [[self.colors allKeys] enumerateObjectsUsingBlock:^(UIColor *possibleColor, NSUInteger idx, BOOL *stop) {
+        CGFloat red2, green2, blue2;
+        [possibleColor getRed:&red2 green:&green2 blue:&blue2 alpha:nil];
+        CGFloat deltaR = pow(red2 - red1, 2);
+        CGFloat deltaG = pow(green2 - green1, 2);
+        CGFloat deltaB = pow(blue2 - blue1, 2);
+        CGFloat delta = deltaR + deltaG + deltaB;
+        if (delta < closestDelta) {
+            closestDelta = delta;
+            closestColor = possibleColor;
+        }
+    }];
+
+    return closestColor;
 }
 
 @end
